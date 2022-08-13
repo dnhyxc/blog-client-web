80ce40107f0c2c840b3a53ddde4e665aa4454371
dnhyxc <2640559435@qq.com>
2022-08-13 23:30:52 +0800

    feat: 代码优化

--- src/components/Image/index.tsx
+++ src/components/Image/index.tsx
@@ -13,28 +13,6 @@ interface IProps {
 }
 
 const Image: React.FC<IProps> = ({ id, url, urls = [], className, onClick }) => {
-  // const [images, setImages] = useState<any[]>([]);
-
-  // useEffect(() => {
-  //   if (urls.length) {
-  //     urls.forEach((src: string) => {
-  //       const img = new window.Image();
-  //       img.src = src;
-  //       img.onload = () => {
-  //         images.push(img);
-  //         setImages([...images]);
-  //       };
-  //     });
-  //   } else {
-  //     const img = new window.Image();
-  //     img.src = url;
-  //     img.onload = () => {
-  //       images.push(img);
-  //       setImages([...images]);
-  //     };
-  //   }
-  // }, []);
-
   const antIcon = <LoadingOutlined style={{ fontSize: 20 }} spin />;
 
   return (
@@ -51,19 +29,6 @@ const Image: React.FC<IProps> = ({ id, url, urls = [], className, onClick }) => 
           <img id={id} src={i} alt="" className={classname(styles.imageItem, className)} />
         ))
       }
-      {/* {urls.length > 0 &&
-        images.length === urls.length &&
-        urls.map((i) => (
-          <img id={id} src={i} alt="" className={classname(styles.imageItem, className)} />
-        ))} */}
-      {/* {!urls.length && images.length > 0 && (
-        <img id={id} src={url} alt="" className={classname(styles.imageItem, className)} />
-      )} */}
-      {/* {!images.length && (
-        <div className={classname(styles.loadingImg, className)}>
-          <Spin size="small" indicator={antIcon} />
-        </div>
-      )} */}
     </div>
   );
 };
