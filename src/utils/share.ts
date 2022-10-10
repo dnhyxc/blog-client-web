e69ecc53e2c0b110e609502a0674c37f480bda55
dengnh <dengninghai@shinemo.com>
2022-10-10 10:36:33 +0800

    feat：优化分享方法

--- src/utils/share.ts
+++ src/utils/share.ts
@@ -9,16 +9,11 @@ export const shareSinaWeiBo = (title: string, pic?: string, url?: string) => {
   const param = {
     url: url || window.location.href,
     type: '3',
-    count: '1',
-    //  是否显示分享数，1显示(可选)
-    appkey: '',
-    //  您申请的应用appkey,显示分享来源(可选)
-    title,
-    // 分享的文字内容(可选，默认为所在页面的title)
-    pic: pic || '',
-    // 分享图片的路径(可选)
-    ralateUid: '',
-    // 关联用户的UID，分享微博会@该用户(可选)
+    count: '1', //  是否显示分享数，1显示(可选)
+    appkey: '', //  您申请的应用appkey,显示分享来源(可选)
+    title, // 分享的文字内容(可选，默认为所在页面的title)
+    pic: pic || '', // 分享图片的路径(可选)
+    ralateUid: '', // 关联用户的UID，分享微博会@该用户(可选)
     rnd: new Date().valueOf(),
   };
   const temp = [];
@@ -29,6 +24,7 @@ export const shareSinaWeiBo = (title: string, pic?: string, url?: string) => {
   window.open(targetUrl, '_blank');
 };
 
+// 分享到QQ空间
 export const shareQZon = (title: string, pic?: string, url?: string) => {
   const param = {
     url: url || window.location.href,
@@ -50,7 +46,7 @@ export const shareQZon = (title: string, pic?: string, url?: string) => {
 };
 
 /**
- * 分享QQ好友
+ * 分享QQ
  * @param  {[type]} title [分享标题]
  * @param  {[type]} url   [分享url链接，默认当前页面链接]
  * @param  {[type]} pic   [分享图片]
@@ -59,16 +55,11 @@ export const shareQZon = (title: string, pic?: string, url?: string) => {
 export const shareQQ = (title: string, pic?: string, url?: string) => {
   const param = {
     url: url || window.location.href,
-    desc: '',
-    // 分享理由
-    title: title || '',
-    // 分享标题(可选)
-    summary: '',
-    // 分享描述(可选)
-    pics: pic || '',
-    // 分享图片(可选)
-    flash: '',
-    // 视频地址(可选)
+    desc: '', // 分享理由
+    title: title || '', // 分享标题(可选)
+    summary: '', // 分享描述(可选)
+    pics: pic || '', // 分享图片(可选)
+    flash: '', // 视频地址(可选)
     site: '', // 分享来源 (可选),
   };
   const s = [];
