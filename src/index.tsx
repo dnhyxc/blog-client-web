4460e6cbe7764c39506cf204de17cf43b9ab242c
dnhyxc <2640559435@qq.com>
2022-11-05 14:51:54 +0800

    feat：调整关于博主UI

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import ReactDOM from 'react-dom/client';
-import Audio from '@/components/Audio';
+// import Audio from '@/components/Audio';
 import App from './router';
 import '@/assets/iconfont/iconfont.css';
 import './index.less';
 
-const audioRoot = ReactDOM.createRoot(document.getElementById('audio') as HTMLElement);
-audioRoot.render(<Audio />);
+// const audioRoot = ReactDOM.createRoot(document.getElementById('audio') as HTMLElement);
+// audioRoot.render(<Audio />);
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(<App />);
