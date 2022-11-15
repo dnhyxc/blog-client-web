42f7397b20cda745a414b0dbb9e110e800595db2
dengnh <dengninghai@shinemo.com>
2022-11-15 22:11:24 +0800

    feat：cover

--- src/view/home/cover/index.tsx
+++ src/view/home/cover/index.tsx
@@ -1,4 +1,6 @@
 import React, { ReactNode } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useStore from '@/store';
 import MIcons from '@/components/Icons';
 import styles from './index.less';
 
@@ -8,8 +10,17 @@ interface IProps {
 }
 
 const Cover: React.FC<IProps> = ({ scrollbarRef, children }) => {
+  const navigate = useNavigate();
+  const {
+    userInfoStore: { getUserInfo },
+  } = useStore();
+
   const toArticleList = () => {
     scrollbarRef?.current.scrollTop(document.body.clientHeight - 49);
+  };
+
+  const toPersonal = () => {
+    navigate('/personal');
   };
 
   return (
@@ -17,8 +28,12 @@ const Cover: React.FC<IProps> = ({ scrollbarRef, children }) => {
       {children}
       <div className={styles.content}>
         <div className={styles.desc}>
-          <div className={styles.authorName}>dnhyxc</div>
-          <div className={styles.line}>行到水穷处，坐看云起时！</div>
+          <div className={styles.authorName} onClick={toPersonal}>
+            {getUserInfo?.username}
+          </div>
+          <div className={styles.line}>
+            {getUserInfo?.motto || '行到水穷处，坐看云起时！'}
+          </div>
         </div>
       </div>
       <div className={styles.downWrap}>
